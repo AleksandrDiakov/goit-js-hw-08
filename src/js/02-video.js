b36ee9d7a730cab5e0d03bb0d7b8ffc1133472c2
Aleksandr Diakov <aldiakov@gmail.com>
2022-02-05 23:43:26 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(getCurrentVideo, 1000));
+
+function getCurrentVideo(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+    .then(function (seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
